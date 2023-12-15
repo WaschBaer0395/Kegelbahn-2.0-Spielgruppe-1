b1102629008edbd45703889b5191fbd431065947
Rene Hippe <rene.hippe@study.hs-duesseldorf.de>
2023-12-15 04:42:33 +0100

    add main menu and run prettier

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from '../sprites/logo.svg';
-import '../styles/App.css';
+import React from 'react'
+import '../styles/App.css'
+import MqttComponent from '../api/mqtt'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="player_list">Players</div>
+        <div className="play_field">
+          <MqttComponent />
+        </div>
+        <div className="progress_bar">Distance</div>
+        <div className="settings_menu">Settings</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Debug.tsx
+++ src/components/Debug.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import '../styles/App.css';
-import{ Client_Subscribe, Client_On_Message} from "../api/mqtt";
+import React from 'react'
+import '../styles/App.css'
+//import{ Client_Subscribe, Client_On_Message} from "../api/mqtt";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <Client_On_Message></Client_On_Message>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  )
 }
 
-export default App;
+export default App
