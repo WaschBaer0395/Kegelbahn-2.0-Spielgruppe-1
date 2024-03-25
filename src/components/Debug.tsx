e6a0683a2a7fdc5d57447d06c537f92f5fab195e
Rene Hippe <rene.hippe@study.hs-duesseldorf.de>
2024-03-25 06:27:32 +0100

    add more options to throw pins

--- src/components/Debug.tsx
+++ src/components/Debug.tsx
@@ -28,17 +28,35 @@ const Debug = () => {
     const handleSensorButtonClick = (sensorNumber: string) => {
         let sensorString = ''
 
-        if(sensorNumber == '0 pins'){
+        if(sensorNumber == '0'){
             sensorString = '{"sensors":[true,true,true,true,true,true,true,true,true],"rounds_played":1,"total_pins_downed":0,"pins_downed":0}'
         }
-        else if (sensorNumber == '2 pins'){
-            sensorString = '{"sensors":[true,false,true,true,false,true,true,true,true],"rounds_played":2,"total_pins_downed":0,"pins_downed":2}'
+        else if (sensorNumber == '1'){
+            sensorString = '{"sensors":[true,true,true,true,true,true,true,true,false],"rounds_played":2,"total_pins_downed":0,"pins_downed":1}'
         }
-        else if (sensorNumber == '5 pins'){
-            sensorString = '{"sensors":[false,true,false,false,true,false,true,true,false],"rounds_played":3,"total_pins_downed":0,"pins_downed":5}'
+        else if (sensorNumber == '2'){
+            sensorString = '{"sensors":[true,true,true,true,true,true,true,false,false],"rounds_played":3,"total_pins_downed":0,"pins_downed":2}'
         }
-        else if (sensorNumber == '9 pins'){
-            sensorString = '{"sensors":[false,false,false,false,false,false,false,false,false],"rounds_played":4,"total_pins_downed":0,"pins_downed":9}'
+        else if (sensorNumber == '3'){
+            sensorString = '{"sensors":[true,true,true,true,true,true,false,false,false],"rounds_played":4,"total_pins_downed":0,"pins_downed":3}'
+        }
+        else if (sensorNumber == '4'){
+            sensorString = '{"sensors":[true,true,true,true,true,false,false,false,false],"rounds_played":5,"total_pins_downed":0,"pins_downed":4}'
+        }
+        else if (sensorNumber == '5'){
+            sensorString = '{"sensors":[true,true,true,true,false,false,false,false,false],"rounds_played":6,"total_pins_downed":0,"pins_downed":5}'
+        }
+        else if (sensorNumber == '6'){
+            sensorString = '{"sensors":[true,true,true,false,false,false,false,false,false],"rounds_played":7,"total_pins_downed":0,"pins_downed":6}'
+        }
+        else if (sensorNumber == '7s'){
+            sensorString = '{"sensors":[true,true,false,false,false,false,false,false,false],"rounds_played":8,"total_pins_downed":0,"pins_downed":7}'
+        }
+        else if (sensorNumber == '8'){
+            sensorString = '{"sensors":[true,false,false,false,false,false,false,false,false],"rounds_played":9,"total_pins_downed":0,"pins_downed":8}'
+        }
+        else if (sensorNumber == '9'){
+            sensorString = '{"sensors":[false,false,false,false,false,false,false,false,false],"rounds_played":10,"total_pins_downed":0,"pins_downed":9}'
         }
         mqttHandler.sendMessage('Kegelbahn/Kegel', sensorString);
     };
@@ -49,13 +67,15 @@ const Debug = () => {
 
             <section className="playerlist-section">
                 <h2>PlayerList</h2>
+                <h4>Send mocked list of players, or empty the current Playerlist</h4>
                 <button onClick={handleSendPlayerList}>Send PlayerList</button>
                 <button onClick={handleEmptyPlayerList}>Empty PlayerList</button>
             </section>
 
             <section className="sensordata-section">
-                <h2>SensorData</h2>
-                {['0 pins', '2 pins', '5 pins', '9 pins'].map((sensorNumber) => (
+                <h2>Throw Pins</h2>
+                <h4>Manually select how many pins have been downed by pressing the correspodning button</h4>
+                {['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].map((sensorNumber) => (
                     <button key={sensorNumber} onClick={() => handleSensorButtonClick(sensorNumber)}>
                         {sensorNumber}
                     </button>
@@ -63,7 +83,7 @@ const Debug = () => {
             </section>
 
             <section className="mqtt-component-section">
-                <h2>MqttComponent</h2>
+                <h2>MQTT Debug Output</h2>
                 <MQTTDebugComponent mqttHandler={mqttHandler} />  {/* Pass the handler as a prop */}
             </section>
         </div>
