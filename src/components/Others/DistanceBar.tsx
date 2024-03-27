1435082e40451742e3d6a4b43beda491b89406b3
Rene Hippe <rene.hippe@study.hs-duesseldorf.de>
2024-03-27 10:33:52 +0100

    eased animation, add env, add flower layers, removed inlet/react-pixi

--- src/components/Others/DistanceBar.tsx
+++ src/components/Others/DistanceBar.tsx
@@ -6,33 +6,41 @@ import '../../styles/DistanceBar.css'
 type DistanceBarProps = {}
 
 export default function DistanceBar({ }: DistanceBarProps) {
-  const game = useGameContext()
-  const [updateFlag, setUpdateFlag] = useState(false);
+    const game = useGameContext()
+    const [updateFlag, setUpdateFlag] = useState(false);
+    const [playerPositions, setPlayerPositions] = useState<number[]>([]);
 
-  useEffect(() => {
-    // Subscribe to score changes in GameLogic and trigger re-render
-    const unsubscribe = game.subscribeToScoreChanges(() => {
-      setUpdateFlag(prevFlag => !prevFlag);
-    });
+    useEffect(() => {
+        // Subscribe to score changes in GameLogic and trigger re-render
+        const unsubscribe = game.subscribeToScoreChanges(() => {
+            setUpdateFlag(prevFlag => !prevFlag);
+        });
 
-    return () => {
-      unsubscribe();
-    };
-  }, [game]);
+        return () => {
+            unsubscribe();
+        };
+    }, [game]);
 
+    useEffect(() => {
+        // Calculate new positions for player pins
+        const newPlayerPositions = game.getPlayers().map(player => {
+            return (((player.getTotalScore() * game.getMultiplier()) + 30) / (game.getMultiplier() * game.getMaxScore())) * 93;
+        });
+        setPlayerPositions(newPlayerPositions);
+    }, [game, updateFlag]);
 
-  return (
-      <div className="beam-chart">
-        <div className="beam"></div>
-        <img src={"src/sprites/finish.png"} className="finish" style={{right: `${0}px`}} alt={"finish Flag"}></img>
-        {game.getPlayers().map((player, index) => (
-            <div key={player.id} className="marker-pin" style={{ left: `${(((player.getTotalScore()*game.getMultiplier())+30)/(game.getMultiplier()*game.getMaxScore()))*93}%`, zIndex: index }}>
-              <div className="marker-pin-circle">
-                <img src={player.spriteLoc + "/preview.png"} alt={player.name} className="avatar" />
-              </div>
-            </div>
-        ))}
-        <img src={"src/sprites/start.png"} className="start" style={{left: `${0}px`}} alt={"start Flag"}></img>
-      </div>
-  )
+    return (
+        <div className="beam-chart">
+            <div className="beam"></div>
+            <img src={"src/sprites/finish.png"} className="finish" style={{ right: `${0}px` }} alt={"finish Flag"}></img>
+            {game.getPlayers().map((player, index) => (
+                <div key={player.id} className="marker-pin" style={{ left: `${playerPositions[index]}%`, zIndex: index , transition: `left ${import.meta.env.VITE_ANIMATIONSPEED}s ease`}}>
+                    <div className="marker-pin-circle">
+                        <img src={player.spriteLoc + "/preview.png"} alt={player.name} className="avatar" />
+                    </div>
+                </div>
+            ))}
+            <img src={"src/sprites/start.png"} className="start" style={{ left: `${0}px` }} alt={"start Flag"}></img>
+        </div>
+    )
 }
--- src/components/Playfield/play_field.tsx
+++ src/components/Playfield/play_field.tsx
@@ -1,19 +1,19 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { GameContext } from "../../api/GameLogicDataContext";
-import { Stage, Sprite } from '@inlet/react-pixi';
-import * as PIXI from 'pixi.js';
+import { Stage, Sprite } from '@pixi/react';
+import PlayerChar from "./player_char";
 
 const PlayField: React.FC = () => {
     const game = useContext(GameContext);
     const [scrollPositionX, setScrollPositionX] = useState(0);
+
 
     useEffect(() => {
         const unsubscribe = game.subscribeToScoreChanges(() => {
             setScrollPositionX(0);
             const targetScrollPosition =
                 game.getPlayers()[game.currentPlayer].scores[game.currentRound - 1] * 1000; //last number here (1000) adjust how far the image will scroll
-
-            const animationDuration = 5000; // 5 seconds
+            const animationDuration = Number(import.meta.env.VITE_ANIMATIONSPEED)*1000;
             const startTime = performance.now();
 
             function animateScroll(currentTime: number) {
@@ -42,11 +42,22 @@ const PlayField: React.FC = () => {
     return (
         <div style={{ width: "100%", height: "100%" }}>
             <Stage width={window.innerWidth} height={window.innerHeight} options={{ backgroundColor: 0x616161 }}>
-                <Sprite texture={PIXI.Texture.from('src/sprites/Background/Background_Layer_Mountains_1_widened.png')} x={-scrollPositionX * 0.08} />
-                <Sprite texture={PIXI.Texture.from('src/sprites/Background/Background_Layer_Mountains_2_widened.png')} x={-scrollPositionX * 0.09} />
-                <Sprite texture={PIXI.Texture.from('src/sprites/Background/Background_Layer_Clouds_widened.png')} x={-scrollPositionX * 0.1} />
-                <Sprite texture={PIXI.Texture.from('src/sprites/Background/Background_Layer_Ground_widened.png')} />
-                <Sprite texture={PIXI.Texture.from('src/sprites/Background/Background_Layer_Flowers_horz_widened.png')}
+                <Sprite image='src/sprites/Background/Background_Layer_Mountains_1_widened.png' x={-scrollPositionX * 0.08} />
+                <Sprite image='src/sprites/Background/Background_Layer_Mountains_2_widened.png' x={-scrollPositionX * 0.09} />
+                <Sprite image='src/sprites/Background/Background_Layer_Clouds_widened.png' x={-scrollPositionX * 0.1} />
+                <Sprite image='src/sprites/Background/Background_Layer_Ground_widened.png' />
+                <Sprite image='src/sprites/Background/Background_Layer_Flowers_widened.png'
+                        rotation={-10.6 * Math.PI / 180} // rotating the horizontal layer
+                        x={-scrollPositionX * 0.19 - 130} // adjusting x pos
+                        y={scrollPositionX * 0.037 + 180} // and adjusting y pos to achieve a diagonal movement
+                />
+                {/*<Sprite image='src/sprites/playerSprites/Male/orange/brown/walking/tile000.png'*/}
+                {/*        x={scrollPositionX * 0.19 - 130 + 150}*/}
+                {/*        y={-scrollPositionX * 0.037 + 180 + 480}*/}
+                {/*        scale={{ x: 2.5, y: 2.5 }} />*/}
+                {/*<div style={{left: `${(scrollPositionX * 0.19 - 130 + 150)}px`, top: `${(-scrollPositionX * 0.037 + 180 + 480)}px`}}></div>*/}
+
+                <Sprite image='src/sprites/Background/Background_Layer_Grass_widened.png'
                         rotation={-10.6 * Math.PI / 180} // rotating the horizontal layer
                         x={-scrollPositionX * 0.2 - 130} // adjusting x pos
                         y={scrollPositionX * 0.038 + 190} // and adjusting y pos to achieve a diagonal movement
